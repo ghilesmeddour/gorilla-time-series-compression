53af217ef7b32cf7ce339880a4b8ec6e24063724
Ghiles Meddour <ghiles.meddour@mobile-devices.fr>
2022-02-12 12:42:19 +0100

    bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## Version 0.2.0 (2022-02-12)
+- Expose `float_format` parameter in `encode_all`. [#3]
+- Add unit test for different float formats.
+
 ## Version 0.1.0 (2021-09-16)
 - Add block size adjustment. [#1]
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="gorillacompression",
-    version="0.1.0",
+    version="0.2.0",
     author="Ghiles Meddour",
     author_email="ghiles.meddour@munic.io",
     description="Gorilla Time Series Compression",
