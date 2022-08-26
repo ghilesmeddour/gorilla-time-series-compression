2ec4be9fd8eb64cb8b38a1c711bcb082d7b4c87f
Ghiles Meddour <ghiles@karmen.io>
2022-08-26 13:27:51 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="gorillacompression",
-    version="0.2.0",
+    version="0.2.1",
     author="Ghiles Meddour",
     author_email="ghiles.meddour@munic.io",
     description="Gorilla Time Series Compression",
